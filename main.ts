8387d82563618c2994df145658ad4fd7082b7fc5
TS <68045330+ATmsPhr10@users.noreply.github.com>
2020-07-23 15:21:17 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,42 +1,15 @@
+let lumen: number[] = []
 let x = 0
 let y = 0
-let r1 = 0
-let r2 = 0
 basic.forever(function () {
+    lumen = [255, 150, 50]
     x = randint(0, 4)
-    y = randint(0, 4)
+    y = 0
     while (y < 8) {
-        if (x == 0) {
-            r1 = 2
-            led.plotBrightness(x, y, 255)
-            led.plotBrightness(x + r1, y, 255)
-            led.plotBrightness(x, y - 1, 150)
-            led.plotBrightness(x + r1, y - 1, 150)
-            led.plotBrightness(x, y - 2, 50)
-            led.plotBrightness(x + r1, y - 2, 50)
-            led.plotBrightness(x, y - 3, 0)
-            led.plotBrightness(x + r1, y - 3, 0)
-            y += 1
-            basic.pause(100)
-        } else if (x == 4) {
-            r2 = 3
-            led.plotBrightness(x, y, 255)
-            led.plotBrightness(x - r2, y, 255)
-            led.plotBrightness(x, y - 1, 150)
-            led.plotBrightness(x - r2, y - 1, 150)
-            led.plotBrightness(x, y - 2, 50)
-            led.plotBrightness(x - r2, y - 2, 50)
-            led.plotBrightness(x, y - 3, 0)
-            led.plotBrightness(x - r2, y - 3, 0)
-            y += 1
-            basic.pause(100)
-        } else {
-            led.plotBrightness(x, y, 255)
-            led.plotBrightness(x, y - 1, 150)
-            led.plotBrightness(x, y - 2, 50)
-            led.plotBrightness(x, y - 3, 0)
-            y += 1
-            basic.pause(100)
+        for (let a = 0; a <= 3; a++) {
+            led.plotBrightness(x, y - a, lumen[a])
         }
+        y += 1
+        basic.pause(200)
     }
 })
